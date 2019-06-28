1e984586259e2fbeafd04ce4182558186a366b7e
Zarif <zarif_41@mail.ru>
2019-06-28 11:46:10 +0300

    add build

---
+++ build/precache-manifest.4a3fbb356a303fb7759d83b87a2fa13f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaeedc00d36abf60b8a4e283e48d67e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de7db7a2d5a2eab29385",
+    "url": "/static/css/main.45152415.chunk.css"
+  },
+  {
+    "revision": "48d082e95eafa89a762a",
+    "url": "/static/js/2.3d16d39c.chunk.js"
+  },
+  {
+    "revision": "de7db7a2d5a2eab29385",
+    "url": "/static/js/main.cf4c5c3d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
